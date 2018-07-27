8f71199948ddf728661b999fa2bb592dc38083e0
scoyne <scoyne@bw.edu>
2018-07-26 20:44:31 -0400

    unsuccessful api authorization

---
+++ bw-connect/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,17 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+
+# bw_connect.rb
+bw = BwConnect.new(
+    # data from config or wherever
+)
+
+# Global factories in Rails
+# Sharing class instance in Rails
--- lib/bw-connect.rb
+++ lib/bw-connect.rb
@@ -1,22 +1,50 @@
-require 'httparty'
-require 'json'
-require 'swagger'
+# require 'httparty'
+# require 'json'
+# require 'swagger'
 
 class Connect
-    include HTTParty
-    include Swagger
+#    include HTTParty
 
-    base_uri 'https://prometheus.bw.edu/CherwellAPI/'
+#    base_uri 'https://prometheus.bw.edu/CherwellAPI/'
 
     # authorizing a Cherwell connecting token to the BWConnect class by creating a new BWConnect client autherized with a username and password
-    def initialize(username, password)
-        response = self.class.post(api_url("token"), body: { username: username, password: password })
-        @client_id = response["client_id"] # used to obtain authorization token require to make the API request to Cherwell
-        raise "Hey Buddy, invalid username or password!" if response.code == 400
+    # client_id: 1b5881d5-be8a-40fe-9f9a-b2a469093569, username: CSDAPI, password: Cherwell4life
+    # def initialize(grant_type, client_id, username, password)
+    def greet()
+        p "Hi here I am"
+        
+#        response = self.class.post 'http://prometheus.bw.edu/CherwellAPI/token?auth_mode=Windows&api_key=1b5881d5-be8a-40fe-9f9a-b2a469093569', body: { grant_type: grant_type, client_id: client_id, username: username, password: password })
+#        @client_id = response["client_id"] # used to obtain authorization token require to make the API request to Cherwell
+#        @access_token = response["access_token"] # Used to obtain auth token required to make API requests
+        
+#        p "Access Token: #{@access_token}"
+
+        # Include Authorization header with this call to `post`
+#        tickets_response = self.class.post(self.api_url('tickets'))
+
+#        raise "Hey Buddy, invalid username or password!" if response.code == 400
     end
 
     # creating endpoint url for a DRY code experience
-    def api_url(endpoint)
-        "https://prometheus.bw.edu/CherwellAPI/api/v1/#{endpoint}"
-    end
+#    def api_url(endpoint)
+#        "https://prometheus.bw.edu/CherwellAPI/api/v1/#{endpoint}"
+#    end
+
+    # def get_tickets
+    #     self.class.post self.api_url('tickets')
+    # end
 end
+
+# Something, somewhere must have this code:
+# grant_type = config.your_api.grant_type
+# c = Connect.new(grant_type, client_id, username, password)
+# c.get_tickets()
+
+
+# # Your Sweet App
+# # tickets_controller.rb#index
+# Ticket.all
+# Connect.get_tickets
+
+# # models/ticket.rb
+# Connect.get_tickets
